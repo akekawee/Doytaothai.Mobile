d9bb8c022f58bc828bf320e9039c786acda5ae13
akekawee <akekawee@switchlink.com.au>
2017-09-22 15:28:03 +1000

    Init

---
+++ DoytaoThai/DoytaoThai/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Xamarin.Forms;
+
+namespace DoytaoThai
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override bool OnBackButtonPressed()
+        {
+            WebView.GoBack();
+            return true;
+        }
+    }
+}
